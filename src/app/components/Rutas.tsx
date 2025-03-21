6fe95687b2183f05f6cdc49bacdbdd7f69036f7b
Romerito1125 <104238155+Romerito1125@users.noreply.github.com>
2025-03-21 11:09:57 -0500

    Update Rutas.tsx

--- src/app/components/Rutas.tsx
+++ src/app/components/Rutas.tsx
@@ -1,3 +1,5 @@
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+
 type Ruta = {
     numero: string;
     nombre: string;
@@ -16,7 +18,14 @@ export default function Rutas({ infoRutas }: RutasProps) {
                     className="p-2 border-b border-gray-300 flex items-center justify-between"
                 >
                     <span className="text-2xl font-bold">{ruta.numero}</span>
-                    <span className="text-lg flex-1 text-right">{ruta.nombre}</span>
+                    
+                    <div className="flex items-center gap-2">
+                        <span className="text-lg">{ruta.nombre}</span>
+                        
+                        <button className="p-1 rounded-full hover:bg-gray-200">
+                            <MagnifyingGlassIcon className="h-8 w-8 text-black" />
+                        </button>
+                    </div>
                 </div>
             ))}
         </div>
