08f007e91a7ad729d7a70563299ee2635f43f4cc
Zuluaga <juanpablozuluaga.0311@gmail.com>
2025-03-21 13:13:23 -0500

    asi

--- src/app/components/Rutas.tsx
+++ src/app/components/Rutas.tsx
@@ -22,10 +22,9 @@ export default function Rutas({ infoRutas }: RutasProps) {
                     <span className="text-lg w-2/4 text-center">{ruta.nombre}</span>
                     
                     <button 
-                        className="w-1/4 flex justify-end items-center gap-2"
+                        className="w-1/4 flex justify-end"
                         aria-label="Buscar ruta"
                     >
-                        <span className="text-sm px-2 py-1 rounded hover:bg-gray-200">Buscar</span>
                         <MagnifyingGlassIcon className="h-8 w-8 text-black hover:bg-gray-200 rounded-full p-1" />
                     </button>
                 </div>
