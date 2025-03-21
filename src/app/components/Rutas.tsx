8327d18ecf4d3c150c1859524da6afec73ce937d
Romerito1125 <104238155+Romerito1125@users.noreply.github.com>
2025-03-21 11:06:53 -0500

    Update Rutas.tsx

--- src/app/components/Rutas.tsx
+++ src/app/components/Rutas.tsx
@@ -7,19 +7,18 @@ type RutasProps = {
     infoRutas: Ruta[];
 };
 
-export default function Rutas({ infoRutas } : RutasProps) {
+export default function Rutas({ infoRutas }: RutasProps) {
     return (
-        <>
-            <div className="cursor-pointer">
-            </div>
+        <div className="w-full">
             {infoRutas.map((ruta) => (
-                <div key={ruta.numero} className="p-2 border-b border-gray-300 flex-row w-full items-center">
-                    <span  className="text-lg tracking-widest font-bold mr-4 leading-none">Número ruta: {ruta.numero}</span>
-                    <div className="text-sm font-mono text-amber-500 tracking-widest leading-tight">Destino ruta: {ruta.nombre}</div>
+                <div 
+                    key={ruta.numero} 
+                    className="p-2 border-b border-gray-300 flex items-center justify-between"
+                >
+                    <span className="text-2xl font-bold">{ruta.numero}</span>
+                    <span className="text-lg flex-1 text-right">{ruta.nombre}</span>
                 </div>
             ))}
-            <div className="cursor-pointer">
-            </div>
-        </>
+        </div>
     );
 }
