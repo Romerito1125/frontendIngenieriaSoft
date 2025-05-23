53b6e201fea30faaf21833d37a481618250802c1
Zulu <juanpablozuluaga.0311@gmail.com>
2025-05-23 09:33:34 -0500

    xd

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      // otras reglas que quieras desactivar
+    },
+  },
 ];
 
 export default eslintConfig;
