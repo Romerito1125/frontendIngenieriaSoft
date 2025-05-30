0c3f146712a0b777fc40283e0bf0ac53c1ba5e8a
Zulu <juanpablozuluaga.0311@gmail.com>
2025-05-29 22:06:16 -0500

    tiempo act

--- src/app/buses-realtime/page.tsx
+++ src/app/buses-realtime/page.tsx
@@ -112,7 +112,7 @@ export default function MapaMIO() {
   useEffect(() => {
     const interval = setInterval(() => {
       if (rutaActiva) obtenerBuses();
-    }, 60000);
+    }, 30000);
     return () => clearInterval(interval);
   }, [rutaActiva, obtenerBuses]);
 
